40a24d424fb994b2d6e49dc72aa2df9efa807c60
Aleksandr <ushniihk@yandex.ru>
2022-03-25 22:31:43 +0300

    feat: add function linkedLast, getTasks, removeNode, remove

--- src/manager/InMemoryHistoryManager.java
+++ src/manager/InMemoryHistoryManager.java
@@ -2,21 +2,77 @@ package tasks.manager;
 
 import tasks.tasks.Task;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 public class InMemoryHistoryManager implements HistoryManager {
-    private List<Task> tasksHistory = new ArrayList<>();
-
+    final private Map<Integer, Node> historyTasks = new HashMap<>();
 
     @Override
     public void add(Task task) {
-        tasksHistory.add(task);
-        if (tasksHistory.size() > 10) tasksHistory.remove(0);
+        if (historyTasks.containsKey(task.getId())) {
+            remove(task.getId());
+        }
+        linkLast(task);
     }
 
     @Override
     public List<Task> getHistory() {
+        return getTasks();
+    }
+
+    private static class Node {
+        Task item;
+        Node next;
+        Node prev;
+
+        Node(Node prev, Task element, Node next) {
+            this.item = element;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    private Node head;
+    private Node tail;
+
+    void linkLast(Task e) {
+        final Node l = tail;
+        final Node newNode = new Node(l, e, null);
+        tail = newNode;
+        if (l == null) {
+            head = newNode;
+        } else {
+            l.next = newNode;
+        }
+        historyTasks.put(e.getId(), newNode);
+    }
+
+    void removeNode(Node n) {
+        Node prev = n.prev;
+        Node next = n.next;
+        if (prev != null) {
+            prev.next = next;
+        } else head = next;
+        if (next != null) {
+            next.prev = prev;
+        } else tail = prev;
+    }
+
+    @Override
+    public void remove(int id) {
+        removeNode(historyTasks.get(id));
+        historyTasks.remove(id);
+    }
+
+    public ArrayList<Task> getTasks() {
+        ArrayList<Task> tasksHistory = new ArrayList<>();
+        if (head != null) {
+            Node first = head;
+            while (first != null) {
+                tasksHistory.add(first.item);
+                first = first.next;
+            }
+        }
         return tasksHistory;
     }
 }
